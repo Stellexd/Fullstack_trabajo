0f4b52d37c431a18f439d2c7724aa7b6d7798042
httpsnt <ro.cantillana@duocuc.cl>
2025-05-23 13:26:33 -0400

    Delete product-service directory
    Error de carga de este archivo

--- product-service/src/main/java/com/ecomarket/productservice/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ecomarket.productservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
